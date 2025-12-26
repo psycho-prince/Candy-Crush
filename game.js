11e8f17d0f723948c5a799742ad9a3492537f025
Prince T Philip <65974917+psycho-prince@users.noreply.github.com>
2025-12-26 19:29:45 +0530

    Update game.js

--- game.js
+++ game.js
@@ -18,7 +18,6 @@ let isProcessing = false;
 let score = 0;
 let timeLeft = 60;
 let gameActive = false;
-let lastMoveTime = 0;
 let timerEvent;
 
 function preload() {
@@ -54,7 +53,7 @@ function setupMenus(scene) {
     let quitBtn = createBtn(scene, config.height/2 + 60, 'QUIT', '#e74c3c', () => location.reload());
     scene.pauseGroup.add([pBg, resBtn, quitBtn]);
 
-    scene.pauseBtn.on('pointerdown', () => { gameActive = false; scene.pauseGroup.setVisible(true); });
+    this.pauseBtn.on('pointerdown', () => { gameActive = false; scene.pauseGroup.setVisible(true); });
 }
 
 function createBtn(scene, y, txt, clr, cb) {
@@ -65,10 +64,11 @@ function createBtn(scene, y, txt, clr, cb) {
 function startLevel(scene, mode) {
     score = 0; timeLeft = 60; gameActive = true;
     scene.menuGroup.setVisible(false);
-    scene.scoreText.setVisible(true).setText('SCORE: 0');
+    scene.scoreText.setVisible(true);
     scene.pauseBtn.setVisible(true);
     if (mode === 'rush') {
         scene.timerText.setVisible(true);
+        if(timerEvent) timerEvent.remove();
         timerEvent = scene.time.addEvent({ delay: 1000, callback: () => { if(gameActive) { timeLeft--; scene.timerText.setText(timeLeft+'s'); if(timeLeft<=0) location.reload(); }}, loop: true });
     }
 }
@@ -93,11 +93,10 @@ function spawnTile(x, y, scene) {
 
 function handleSelect(tile, scene) {
     if (isProcessing || !gameActive) return;
-    lastMoveTime = scene.time.now;
 
     if (!selectedTile) {
         selectedTile = tile;
-        tile.setAlpha(0.6);
+        tile.setAlpha(0.5);
     } else {
         let x1 = selectedTile.getData('gridX'), y1 = selectedTile.getData('gridY');
         let x2 = tile.getData('gridX'), y2 = tile.getData('gridY');
@@ -109,7 +108,7 @@ function handleSelect(tile, scene) {
         } else {
             selectedTile.setAlpha(1);
             selectedTile = tile;
-            tile.setAlpha(0.6);
+            tile.setAlpha(0.5);
         }
     }
 }
@@ -125,17 +124,17 @@ function swapTiles(t1, t2, scene) {
         targets: [t1, t2],
         x: (t) => t.getData('gridX') * TILE_SIZE + TILE_SIZE,
         y: (t) => t.getData('gridY') * TILE_SIZE + 200,
-        duration: 200,
+        duration: 250,
         onComplete: () => {
             if (!checkMatches(scene)) {
-                // Swap back if no match
+                // Swap Back
                 grid[y1][x1] = t1; grid[y2][x2] = t2;
                 t1.setData({gridX: x1, gridY: y1}); t2.setData({gridX: x2, gridY: y2});
                 scene.tweens.add({
                     targets: [t1, t2],
                     x: (t) => t.getData('gridX') * TILE_SIZE + TILE_SIZE,
                     y: (t) => t.getData('gridY') * TILE_SIZE + 200,
-                    duration: 200,
+                    duration: 250,
                     onComplete: () => { isProcessing = false; selectedTile = null; }
                 });
             } else {
@@ -146,31 +145,32 @@ function swapTiles(t1, t2, scene) {
 }
 
 function checkMatches(scene) {
-    let toClear = new Set();
+    let toClear = [];
     for (let y = 0; y < GRID_SIZE; y++) {
         for (let x = 0; x < GRID_SIZE - 2; x++) {
-            if (grid[y][x] && grid[y][x+1] && grid[y][x+2]) {
-                if (grid[y][x].getData('color') === grid[y][x+1].getData('color') && grid[y][x].getData('color') === grid[y][x+2].getData('color')) {
-                    toClear.add(grid[y][x]); toClear.add(grid[y][x+1]); toClear.add(grid[y][x+2]);
-                }
+            let t1 = grid[y][x], t2 = grid[y][x+1], t3 = grid[y][x+2];
+            if (t1 && t2 && t3 && t1.getData('color') === t2.getData('color') && t1.getData('color') === t3.getData('color')) {
+                toClear.push(t1, t2, t3);
             }
         }
     }
     for (let x = 0; x < GRID_SIZE; x++) {
         for (let y = 0; y < GRID_SIZE - 2; y++) {
-            if (grid[y][x] && grid[y+1][x] && grid[y+2][x]) {
-                if (grid[y][x].getData('color') === grid[y+1][x].getData('color') && grid[y][x].getData('color') === grid[y+2][x].getData('color')) {
-                    toClear.add(grid[y][x]); toClear.add(grid[y+1][x]); toClear.add(grid[y+2][x]);
-                }
+            let t1 = grid[y][x], t2 = grid[y+1][x], t3 = grid[y+2][x];
+            if (t1 && t2 && t3 && t1.getData('color') === t2.getData('color') && t1.getData('color') === t3.getData('color')) {
+                toClear.push(t1, t2, t3);
             }
         }
     }
-    if (toClear.size > 0) {
+
+    if (toClear.length > 0) {
         scene.sound.play('pop');
         toClear.forEach(t => { 
-            grid[t.getData('gridY')][t.getData('gridX')] = null; 
-            t.destroy(); 
-            score += 10;
+            if (grid[t.getData('gridY')][t.getData('gridX')]) {
+                grid[t.getData('gridY')][t.getData('gridX')] = null;
+                t.destroy();
+                score += 10;
+            }
         });
         scene.scoreText.setText('SCORE: ' + score);
         return true;
@@ -179,6 +179,8 @@ function checkMatches(scene) {
 }
 
 function handleGravity(scene) {
+    let longestFall = 0;
+
     for (let x = 0; x < GRID_SIZE; x++) {
         let emptySpaces = 0;
         for (let y = GRID_SIZE - 1; y >= 0; y--) {
@@ -189,25 +191,36 @@ function handleGravity(scene) {
                 grid[y + emptySpaces][x] = tile;
                 grid[y][x] = null;
                 tile.setData('gridY', y + emptySpaces);
-                scene.tweens.add({ targets: tile, y: (y + emptySpaces) * TILE_SIZE + 200, duration: 300 });
+                
+                scene.tweens.add({
+                    targets: tile,
+                    y: (y + emptySpaces) * TILE_SIZE + 200,
+                    duration: 300,
+                    ease: 'Power1'
+                });
+                longestFall = 300;
             }
         }
+        // Refill
         for (let i = 0; i < emptySpaces; i++) {
             let t = spawnTile(x, i, scene);
-            grid[i][x] = t; t.y = -100;
+            grid[i][x] = t;
+            t.y = -100;
             scene.tweens.add({ targets: t, y: i * TILE_SIZE + 200, duration: 400 });
+            longestFall = 400;
         }
     }
-    scene.time.delayedCall(500, () => {
-        if (checkMatches(scene)) handleGravity(scene);
-        else { isProcessing = false; selectedTile = null; }
+
+    // Wait for the longest animation to finish before checking for new matches
+    scene.time.delayedCall(longestFall + 50, () => {
+        if (checkMatches(scene)) {
+            handleGravity(scene);
+        } else {
+            isProcessing = false;
+            selectedTile = null;
+        }
     });
 }
 
-function update(time) {
-    if (gameActive && time - lastMoveTime > 4000) {
-        let t = grid[Phaser.Math.Between(0,9)][Phaser.Math.Between(0,9)];
-        if (t) { this.tweens.add({ targets: t, angle: 10, yoyo: true, duration: 100, repeat: 3 }); lastMoveTime = time; }
-    }
-}
-    
+function update() {}
+
